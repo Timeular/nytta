4359a1d74fdf34879ecb16a814fe0b9d9d36b3de
Stefan Puchmann <stefan.puchmann@gmail.com>
2021-12-03 21:12:39 +0100

    Dependency Upgrade

--- spring-ext/src/test/kotlin/com/timeular/nytta/spring/flywaydb/FlywayTest.kt
+++ spring-ext/src/test/kotlin/com/timeular/nytta/spring/flywaydb/FlywayTest.kt
@@ -4,12 +4,10 @@ import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
 import org.dbunit.DataSourceDatabaseTester
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.annotation.Configuration
-import org.springframework.test.context.junit.jupiter.SpringExtension
 import javax.sql.DataSource
 
 @Configuration
@@ -19,7 +17,6 @@ open class FlywayTestApplication
 @SpringBootTest(classes = [
     FlywayTestApplication::class
 ])
-@ExtendWith(SpringExtension::class)
 class FlywayTest(
         @Autowired
         private val dataSource: DataSource
