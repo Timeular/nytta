be4219484e7169fea7138fe43adb4a9e88d0cbc9
Stefan Puchmann <stefan.puchmann@gmail.com>
2023-10-03 15:04:43 +0200

    Adding locale utils

--- email/core/src/main/kotlin/com/timeular/nytta/email/core/MailService.kt
+++ email/core/src/main/kotlin/com/timeular/nytta/email/core/MailService.kt
@@ -8,54 +8,78 @@ interface MailService {
     fun sendMail(mailConfig: MailConfig): Boolean
 
     fun sendMail(
-            mailTemplate: MailTemplate,
-            mailContext: Map<String, Any>,
-            receiver: Set<MailContact>,
-            locale: Locale = Locale.ENGLISH
+        mailTemplate: MailTemplate,
+        mailContext: Map<String, Any>,
+        receiver: Set<MailContact>,
     ): Boolean = sendMail(
-            mailTemplate = mailTemplate,
-            mailContext = mailContext,
-            receiver = receiver,
-            deliveryTime = null,
-            locale = locale
+        mailTemplate = mailTemplate,
+        mailContext = mailContext,
+        receiver = receiver,
+        locale = Locale.ENGLISH
     )
 
     fun sendMail(
-            mailTemplate: MailTemplate,
-            mailContext: Map<String, Any>,
-            receiver: Set<MailContact>,
-            deliveryTime: ZonedDateTime,
-            locale: Locale = Locale.ENGLISH
+        mailTemplate: MailTemplate,
+        mailContext: Map<String, Any>,
+        receiver: Set<MailContact>,
+        locale: Locale = Locale.ENGLISH
     ): Boolean = sendMail(
-            mailTemplate = mailTemplate,
-            mailContext = mailContext,
-            receiver = receiver,
-            deliveryTime = deliveryTime,
-            inlineAttachments = emptyList(),
-            locale = locale
+        mailTemplate = mailTemplate,
+        mailContext = mailContext,
+        receiver = receiver,
+        deliveryTime = null,
+        locale = locale
     )
 
     fun sendMail(
-            mailTemplate: MailTemplate,
-            mailContext: Map<String, Any>,
-            receiver: Set<MailContact>,
-            inlineAttachments: List<Attachment> = emptyList(),
-            locale: Locale = Locale.ENGLISH
+        mailTemplate: MailTemplate,
+        mailContext: Map<String, Any>,
+        receiver: Set<MailContact>,
+        deliveryTime: ZonedDateTime
     ): Boolean = sendMail(
-            mailTemplate = mailTemplate,
-            mailContext = mailContext,
-            receiver = receiver,
-            inlineAttachments = inlineAttachments,
-            deliveryTime = null,
-            locale = locale
+        mailTemplate = mailTemplate,
+        mailContext = mailContext,
+        receiver = receiver,
+        deliveryTime = deliveryTime,
+        locale = Locale.ENGLISH
     )
 
     fun sendMail(
-            mailTemplate: MailTemplate,
-            mailContext: Map<String, Any>,
-            receiver: Set<MailContact>,
-            deliveryTime: ZonedDateTime? = null,
-            inlineAttachments: List<Attachment> = emptyList(),
-            locale: Locale = Locale.ENGLISH
+        mailTemplate: MailTemplate,
+        mailContext: Map<String, Any>,
+        receiver: Set<MailContact>,
+        deliveryTime: ZonedDateTime,
+        locale: Locale = Locale.ENGLISH
+    ): Boolean = sendMail(
+        mailTemplate = mailTemplate,
+        mailContext = mailContext,
+        receiver = receiver,
+        deliveryTime = deliveryTime,
+        inlineAttachments = emptyList(),
+        locale = locale
+    )
+
+    fun sendMail(
+        mailTemplate: MailTemplate,
+        mailContext: Map<String, Any>,
+        receiver: Set<MailContact>,
+        inlineAttachments: List<Attachment> = emptyList(),
+        locale: Locale = Locale.ENGLISH
+    ): Boolean = sendMail(
+        mailTemplate = mailTemplate,
+        mailContext = mailContext,
+        receiver = receiver,
+        inlineAttachments = inlineAttachments,
+        deliveryTime = null,
+        locale = locale
+    )
+
+    fun sendMail(
+        mailTemplate: MailTemplate,
+        mailContext: Map<String, Any>,
+        receiver: Set<MailContact>,
+        deliveryTime: ZonedDateTime? = null,
+        inlineAttachments: List<Attachment> = emptyList(),
+        locale: Locale = Locale.ENGLISH
     ): Boolean
 }
